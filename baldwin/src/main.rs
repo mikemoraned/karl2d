f85822da4fe3c95c6a4b5f4e39801801eaae1044
Mike Moran <lazysurefix@gmail.com>
2018-08-02 00:27:50 +0200

    WIP

--- baldwin/src/main.rs
+++ baldwin/src/main.rs
@@ -6,6 +6,8 @@ use genevo::population::ValueEncodedGenomeBuilder;
 use genevo::prelude::*;
 use genevo::types::fmt::Display;
 
+use std::cmp;
+
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 enum PathElement {
     Forward,
@@ -13,6 +15,7 @@ enum PathElement {
 }
 
 type Genotype = Vec<u8>;
+
 type Path = Vec<PathElement>;
 type Phenotype = Path;
 
@@ -34,13 +37,23 @@ impl AsPhenotype for Genotype {
 }
 
 #[derive(Clone)]
-struct FitnessCalc {
-    goal_distance : usize,
+struct Goal {
+    target_length : usize,
 }
 
-impl FitnessFunction<Genotype, usize> for FitnessCalc {
+#[derive(Clone, Copy)]
+struct FitnessCalc<'a> {
+    goal: &'a Goal,
+}
+
+impl<'a> FitnessFunction<Genotype, usize> for FitnessCalc<'a> {
     fn fitness_of(&self, genome: &Genotype) -> usize {
-        0
+        let length = genome.as_path().iter()
+            .fold(0, |acc, e| acc + match e {
+                PathElement::Forward => 1,
+                _ => 0
+            });
+        cmp::min(self.goal.target_length, length)
     }
 
     fn average(&self, fitness_values: &[usize]) -> usize {
@@ -48,7 +61,7 @@ impl FitnessFunction<Genotype, usize> for FitnessCalc {
     }
 
     fn highest_possible_fitness(&self) -> usize {
-        100_00
+        self.goal.target_length
     }
 
     fn lowest_possible_fitness(&self) -> usize {
@@ -59,12 +72,29 @@ impl FitnessFunction<Genotype, usize> for FitnessCalc {
 fn main() {
     println!("Hello, world!");
 
-    let target_length = 200;
+    let goal : Goal = Goal {
+        target_length: 200,
+    };
 
     let population_size = 100;
 
     let initial_population: Population<Genotype> = build_population()
-        .with_genome_builder(ValueEncodedGenomeBuilder::new(target_length, 0, 1))
+        .with_genome_builder(ValueEncodedGenomeBuilder::new(goal.target_length, 0, 1))
         .of_size(population_size)
         .uniform_at_random();
+
+    let fitness_calc = FitnessCalc { goal: &goal };
+
+    genetic_algorithm()
+        .with_evaluation(fitness_calc)
+        .with_selection(MaximizeSelector::new(0.7, 2))
+        .with_crossover(MultiPointCrossBreeder::new(goal.target_length / 6))
+        .with_mutation(RandomValueMutator::new(0.01, 0, 1))
+        .with_reinsertion(ElitistReinserter::new(
+            fitness_calc,
+            true,
+            0.7,
+        ))
+        .with_initial_population(initial_population)
+        .build();
 }
